d80ed23627af6bc7eb0fb7bc20f6ad7eb7970663
mihanizm56 <m727507.56@mail.ru>
2020-12-19 05:45:17 +0300

    build(configure-project): update problems

--- src/pages/login/route.ts
+++ src/pages/login/route.ts
@@ -2,6 +2,6 @@ import { LOGIN_ROUTE_NAME } from '@/_constants/routes';
 
 export default {
   name: LOGIN_ROUTE_NAME,
-  path: '/login',
+  path: '/test-login-form',
   loadAction: () => import('./index'),
 };
